7348982a952feb67e7e686291eafc66c3975b7f4
Vinicius Rodrigues <viniciuscardosorodrigues@gmail.com>
2024-11-08 23:33:58 -0300

    Atualizacao

--- backend/app.py
+++ backend/app.py
@@ -292,4 +292,4 @@ def generate_pdf():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 db_config = {
-    'host': '54.233.14.188',
+    'host': 'localhost',
     'user': 'mstarsupply_user',
     'password': 'LHT$85B67#Xp',
     'database': 'mstarsupply'
